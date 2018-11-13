0020be64d8c3a8d5461add497f3e855237941a1f
Kundan Kumar <krkundankumar156@gmail.com>
2018-11-13 11:12:34 -0800

    added new file home.html

---
+++ home.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import { NavController, AlertController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  public image:string;
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController,private camera:Camera) {
+
+  }
+
+  takepicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      saveToPhotoAlbum: true,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.image = 'data:image/jpeg;base64,' + imageData;
+      }, (err) => {
+        this.displayErrorAlert(err);
+      });
+  }
+
+  displayErrorAlert(err){
+    console.log(err);
+    let alert = this.alertCtrl.create({
+       title: 'Error',
+       subTitle: 'Error while trying to capture picture',
+       buttons: ['OK']
+     });
+     alert.present();
+  }
+
+
+  getpicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+       saveToPhotoAlbum: false
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.image = 'data:image/jpeg;base64,' + imageData;
+      }, (err) => {
+        this.displaygetErrorAlert(err);
+      });
+  }
+
+  displaygetErrorAlert(err){
+    console.log(err);
+    let alert = this.alertCtrl.create({
+       title: 'Error',
+       subTitle: 'Error while trying to capture picture',
+       buttons: ['OK']
+     });
+     alert.present();
+  }
+
+}
